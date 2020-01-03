7fb4c9be3a5e4ae04c41e225c5707fc5c9b54b46
andy <ztao8607@gmail.com>
2020-01-03 15:29:45 +0800

    project init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module tio-monitor
+
+go 1.13
+
+require (
+	github.com/golang/mock v1.3.1
+	github.com/hashicorp/consul/api v1.3.0
+	github.com/prometheus/client_golang v1.3.0
+	github.com/prometheus/common v0.7.0
+	github.com/sirupsen/logrus v1.4.2
+	github.com/tio-serverless/grpc v0.0.0-20200103071801-bc1d06d17b68
+	google.golang.org/grpc v1.26.0
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	mi, err := NewMonImplement()
+	if err != nil {
+		logrus.Fatalf("Monitor Init Error. %s", err.Error())
+	}
+
+	allSvc, err := mi.WatchProemetheus()
+	if err != nil {
+		logrus.Fatalf("Create Prometheus Watch Chan Error. %s", err.Error())
+	}
+
+	go func() {
+		for {
+			select {
+			case svc := <-allSvc:
+				for _, s := range svc {
+					err := mi.WatchForScala(s)
+					if err != nil {
+						logrus.Errorf("Watch Scala %v Error. %s", s, err.Error())
+					}
+				}
+
+			}
+		}
+
+	}()
+
+	logrus.Infof("Monitor Service Start - - - ")
+	start(mi, 80)
+}
+
+func init() {
+	switch strings.ToLower(os.Getenv("TIO_MONITOR_LOG_LEVEL")) {
+	case "info":
+		logrus.SetLevel(logrus.InfoLevel)
+	case "warn":
+		logrus.SetLevel(logrus.WarnLevel)
+	case "error":
+		logrus.SetLevel(logrus.ErrorLevel)
+	default:
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+}
